8053f1b2949dc7bf24710201e45268fb495e6f96
LGC363 <2220801450@qq.com>
2023-11-09 19:17:02 +0800

    updata

--- main.cpp
+++ main.cpp
@@ -35,23 +35,22 @@ struct Shader : IShader {
         vec3 bn = (varying_nrm*bar).normalized(); // per-vertex normal interpolation
         vec2 uv = varying_uv*bar; // tex coord interpolation
 
-        // for the math refer to the tangent space normal mapping lecture
-        // https://github.com/ssloy/tinyrenderer/wiki/Lesson-6bis-tangent-space-normal-mapping
+
         mat<3,3> AI = mat<3,3>{ {view_tri.col(1) - view_tri.col(0), view_tri.col(2) - view_tri.col(0), bn} }.invert();
         vec3 i = AI * vec3{varying_uv[0][1] - varying_uv[0][0], varying_uv[0][2] - varying_uv[0][0], 0};
         vec3 j = AI * vec3{varying_uv[1][1] - varying_uv[1][0], varying_uv[1][2] - varying_uv[1][0], 0};
         mat<3,3> B = mat<3,3>{ {i.normalized(), j.normalized(), bn} }.transpose();
 
-        vec3 n = (B * model.normal(uv)).normalized(); // transform the normal from the texture to the tangent space
-        double diff = std::max(0., n*uniform_l); // diffuse light intensity
-        vec3 r = (n*(n*uniform_l)*2 - uniform_l).normalized(); // reflected light direction, specular mapping is described here: https://github.com/ssloy/tinyrenderer/wiki/Lesson-6-Shaders-for-the-software-renderer
-        double spec = std::pow(std::max(-r.z, 0.), 5+sample2D(model.specular(), uv)[0]); // specular intensity, note that the camera lies on the z-axis (in view), therefore simple -r.z
+        vec3 n = (B * model.normal(uv)).normalized(); 
+        double diff = std::max(0., n*uniform_l);
+        vec3 r = (n*(n*uniform_l)*2 - uniform_l).normalized(); 
+        double spec = std::pow(std::max(-r.z, 0.), 5+sample2D(model.specular(), uv)[0]); 
 
         TGAColor c = sample2D(model.diffuse(), uv);
         for (int i : {0,1,2})
-            gl_FragColor[i] = std::min<int>(10 + c[i]*(diff + spec), 255); // (a bit of ambient light, diff + spec), clamp the result
+            gl_FragColor[i] = std::min<int>(10 + c[i]*(diff + spec), 255); 
 
-        return false; // the pixel is not discarded
+        return false;
     }
 };
 
